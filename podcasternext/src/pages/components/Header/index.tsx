12f31b09580f7e203780c7d91f84f9c79fa21c67
Guilherme Jordão <62813340+jordaomoraes@users.noreply.github.com>
2021-05-03 08:04:48 -0300

    inicio

---
+++ podcasternext/src/pages/components/Header/index.tsx
@@ -0,0 +1,22 @@
+import format from 'date-fns/format';
+import ptBR from 'date-fns/locale/pt-BR';
+import ptBr from 'date-fns/locale/pt-BR';
+
+import styles from './styles.module.scss';
+
+export function Header() {
+  //Para formatar a data (cada biblioteca tem uma string de formatação)
+  const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
+    locale: ptBR,
+  });
+
+  return (
+    <header className={ styles.headerContainer }>
+      <img src="/logo.svg" alt="Podcastr"/>
+
+      <p>O melhor para você ouvir, aaa sempre</p>
+
+      <span>{currentDate}</span>
+    </header>
+  );
+}
